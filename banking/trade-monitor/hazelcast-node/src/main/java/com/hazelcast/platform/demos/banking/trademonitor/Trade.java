107b7a419d402e652718b4dc4c1a8d3c24c2b404
Neil Stevenson <neilstevenson@users.noreply.github.com>
2020-02-23 22:02:48 +0000

    Domain object

---
+++ banking/trade-monitor/hazelcast-node/src/main/java/com/hazelcast/platform/demos/banking/trademonitor/Trade.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2008-2020, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.platform.demos.banking.trademonitor;
+
+import java.io.Serializable;
+
+/**
+ * XXX
+ */
+@SuppressWarnings("serial")
+public class Trade implements Serializable {
+
+    private String id;
+    private long timestamp;
+    private String symbol;
+    private int quantity;
+    private int price;
+
+    // Generated getters/setters and toString()
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Trade [id=" + id + ", timestamp=" + timestamp + ", symbol=" + symbol + ", quantity=" + quantity
+                + ", price=" + price + "]";
+    }
+
+}
