84a5592140cd4988e4bbc486d9f4a08080c0c298
Neil Stevenson <neilstevenson@users.noreply.github.com>
2020-04-18 16:55:18 +0100

    Initial

---
+++ banking/credit-value-adjustment/src/main/scripts/docker-webapp.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+PROJECT=cva
+MODULE=webapp
+
+BASEDIR=`dirname $0`
+cd $BASEDIR/../../../$MODULE
+
+HOST_IP=`ifconfig | grep -w inet | grep -v 127.0.0.1 | cut -d" " -f2`
+if [ "$HOST_IP" == "" ]
+then
+ HOST_IP=127.0.0.1
+fi
+if [ `echo $HOST_IP | wc -w` -ne 1 ]
+then
+ echo \$HOST_IP unclear:
+ ifconfig | grep -w inet | grep -v 127.0.0.1
+ exit 1
+fi
+
+DOCKER_IMAGE=hazelcast-${PROJECT}/${MODULE}
+
+CMD="docker run -e MY_KUBERNETES_ENABLED=false -e JAVA_ARGS=-Dhazelcast.local.publicAddress=${HOST_IP}:5701 -p 8080:8080 ${DOCKER_IMAGE} $@"
+#echo $CMD
+
+$CMD
+RC=$?
+echo RC=${RC}
+exit ${RC}
+
