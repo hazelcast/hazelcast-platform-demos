993cef41960c9efd80fb50660f71298274f8a6d2
Neil Stevenson <neilstevenson@users.noreply.github.com>
2020-04-22 15:33:52 +0100

    Initial

--- banking/credit-value-adjustment/webapp/src/main/app/src/App.js
+++ banking/credit-value-adjustment/webapp/src/main/app/src/App.js
@@ -1,15 +1,8 @@
-'use strict';
-
 import React, { Component } from 'react';
 import Fixings from './components/fixings'
 import Jobs from './components/jobs'
 
 class App extends Component {
-
-	constructor(props) {
-		super(props);
-	}
-
 	render() {
 		return (
 				<div>
--- banking/credit-value-adjustment/webapp/src/main/app/src/components/fixings/fixings.js
+++ banking/credit-value-adjustment/webapp/src/main/app/src/components/fixings/fixings.js
@@ -1,12 +1,6 @@
-'use strict';
-
 import React, { Component } from 'react';
 
 class Fixings extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
         	<div class="minor_pane">
