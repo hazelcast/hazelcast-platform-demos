5f35fff649db47d71ef29b72b8c47da68d0be971
Neil Stevenson <neilstevenson@users.noreply.github.com>
2020-11-19 17:25:39 +0000

    Initial

--- telco/churn/data-feed/src/main/java/com/hazelcast/platform/demos/telco/churn/ApplicationRunner.java
+++ telco/churn/data-feed/src/main/java/com/hazelcast/platform/demos/telco/churn/ApplicationRunner.java
@@ -16,6 +16,7 @@
 
 package com.hazelcast.platform.demos.telco.churn;
 
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -32,18 +33,21 @@ import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hazelcast.platform.demos.telco.churn.domain.CallDataRecord;
+import com.hazelcast.platform.demos.telco.churn.testdata.CustomerTestdata;
 
 /**
- * XXX
+ * <p>Every second, a different customer attempts a call, and some calls are dropped.
+ * </p>
  */
 @Configuration
 public class ApplicationRunner implements CommandLineRunner {
     private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationRunner.class);
     private static final int DEFAULT_BATCH_SIZE = 100;
-    private static final long LOG_THRESHOLD = 10_000L;
-    private static final int MAX_BATCH_SIZE = 16 * 1024;
+    private static final long LOG_THRESHOLD = 60L;
+    private static final int FIVE = 5;
+    private static final int TEN = 10;
+    private static final long MILLIS_TO_SECONDS = 1000L;
 
-    //XXX private static CountDownLatch countDownLatch;
     private static AtomicLong onSuccessCount = new AtomicLong(0);
     private static AtomicLong onFailureCount = new AtomicLong(0);
 
@@ -59,96 +63,107 @@ public class ApplicationRunner implements CommandLineRunner {
     private KafkaTemplate<String, String> kafkaTemplate;
 
     /**
-     * <p>Produce a batch of 100 CDRs at once, then wait a second and
-     * repeat. For an alternative approach, to produce these evenly
-     * spread throughout the second interval, see the {@code Trade Monitor}
-     * <a href="https://github.com/hazelcast/hazelcast-platform-demos/blob/
-master/banking/trade-monitor/trade-producer/
-src/main/java/com/hazelcast/platform/demos/banking/trademonitor/ApplicationRunner.java#L97">here</a>
-     * which will produce one record every .00333 seconds to output 300/second.
+     * <p>Create a CDR for a customer, then loop round the next customer,
+     * forever.
      * </p>
      */
     @Override
     public void run(String... args) throws Exception {
         LOGGER.info("-=-=-=-=- START {} START -=-=-=-=-=-", this.springApplicationName);
 
-        this.setBatchSize(args);
-        LOGGER.info("Producing {} call records in a batch per second", this.batchSize);
+        int accounts = CustomerTestdata.getFirstNames().length
+                * CustomerTestdata.getLastNames().length;
 
-        long count = 0;
-        long now = System.currentTimeMillis();
+        LOGGER.info("Producing {} call records one per second", this.batchSize);
+
+        int insert = 0;
         long reportEvery = 1;
 
         // create some CDRs, wait, repeat
         try {
             while (true) {
-                for (int i = 0; i < this.batchSize; i++) {
+                int account = insert % accounts;
+                long now = System.currentTimeMillis();
 
-                    int id = Integer.MIN_VALUE + i;
+                CallDataRecord cdr = makeCallDataRecord(account, accounts, now, insert);
 
-                    CallDataRecord cdr = new CallDataRecord();
-                    cdr.setId("id" + id);
-                    cdr.setCallerTelno("telno" + id);
-                    cdr.setCallerMastId("i" + id);
-                    cdr.setCalleeTelno("i" + id);
-                    cdr.setCalleeMastId("i" + id);
-                    cdr.setDurationSeconds(0);
-                    cdr.setStartTimestamp(0L);
-                    cdr.setCallSuccessful(true);
-                    cdr.setCreatedBy(this.springApplicationName);
-                    cdr.setCreatedDate(now);
-                    cdr.setLastModifiedBy(this.springApplicationName);
-                    cdr.setLastModifiedDate(now);
-
-                    // Report 0, 2, 4, 8, 16... until every 10,000
-                    if (count == 0 || count % reportEvery == 0) {
-                        if (reportEvery < LOG_THRESHOLD) {
-                            reportEvery += reportEvery;
-                        }
-                        this.write(cdr, count, true);
-                    } else {
-                        this.write(cdr, count, false);
+                // Report 0, 2, 4, 8, 16... until every 60
+                if (insert == 0 || insert % reportEvery == 0) {
+                    if (reportEvery < LOG_THRESHOLD) {
+                        reportEvery += reportEvery;
                     }
-                    count++;
+                    this.write(cdr, insert, true);
+                } else {
+                    this.write(cdr, insert, false);
                 }
 
+                insert++;
                 TimeUnit.SECONDS.sleep(1L);
             }
         } catch (InterruptedException exception) {
+            long good = onSuccessCount.get();
+            long bad = onFailureCount.get();
+            if (bad != 0) {
+                LOGGER.error("Interrupted, {} bad from {}", bad, insert);
+            } else {
+                if ((bad + good) != insert) {
+                    // Could be some in-flight
+                    LOGGER.warn("Interrupted, {} bad {} good, from {} inserts",
+                            bad, good, insert);
+                } else {
+                    LOGGER.info("Interrupted, {} inserts", insert);
+                }
+            }
+
         }
 
         LOGGER.info("-=-=-=-=-  END  {}  END  -=-=-=-=-=-", this.springApplicationName);
     }
 
+
     /**
-     * <p>Possibly change batch size with command line argument.
+     * <p>Create a "<i>random</i>" (but actually deterministic) call.
      * </p>
      *
-     * @param args
+     * @param account Whose call
+     * @param accounts
+     * @param now When the CDR is recorded, after when it ended
+     * @param call Sequence number for calls generated
+     * @return
      */
-    private void setBatchSize(String... args) {
-        if (args.length > 0) {
-            try {
-                int i = Integer.parseInt(args[0]);
-                if (i > 0) {
-                    if (i == DEFAULT_BATCH_SIZE) {
-                        LOGGER.warn("Supplied batch size {} same as default", i);
-                    } else {
-                        if (i < MAX_BATCH_SIZE) {
-                            LOGGER.info("Using {} from args as batch size", i);
-                            this.batchSize = i;
-                        } else {
-                            LOGGER.warn("Supplied batch size {} > max allowed {}", i, MAX_BATCH_SIZE);
-                        }
-                    }
-                } else {
-                    LOGGER.error("Ignoring {} from args as batch size", i);
-                }
-                return;
-            } catch (NumberFormatException nfe) {
-                LOGGER.error("Arg '{}' provided for batch size", args[0]);
-            }
+    private CallDataRecord makeCallDataRecord(int account, int accounts, long now, int call) {
+        CallDataRecord cdr = new CallDataRecord();
+        UUID uuid = new UUID(Long.valueOf(account), Long.valueOf(call));
+        cdr.setId(uuid.toString());
+
+        String myTelno = MyUtils.getTelno(account);
+        cdr.setCallerTelno(myTelno);
+        cdr.setCallerMastId(MyUtils.getMastId(account, call));
+
+        // Call to a pseudo-random number beyond the range of accounts, so not calling self
+        int callee = account + accounts + call;
+        cdr.setCalleeTelno(MyUtils.getTelno(callee));
+        cdr.setCalleeMastId(MyUtils.getMastId(callee, call));
+
+        // Fake every 5th call as unsuccessful.
+        if (call % FIVE == 0) {
+            cdr.setDurationSeconds(0);
+            cdr.setCallSuccessful(false);
+        } else {
+            cdr.setDurationSeconds(TEN + call);
+            cdr.setCallSuccessful(true);
         }
+
+        // Start time is before now, based on call duration plus a lag
+        long when = now - ((cdr.getDurationSeconds() + FIVE) * MILLIS_TO_SECONDS);
+        cdr.setStartTimestamp(when);
+
+        cdr.setCreatedBy(this.springApplicationName);
+        cdr.setCreatedDate(now);
+        cdr.setLastModifiedBy(this.springApplicationName);
+        cdr.setLastModifiedDate(now);
+
+        return cdr;
     }
 
 
