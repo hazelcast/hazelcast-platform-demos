5ab5778a360cf7768aef7b24d8263f786925c595
Neil Stevenson <neilstevenson@users.noreply.github.com>
2022-07-15 17:07:23 +0100

    Initial

---
+++ banking/trade-monitor/finos-nodejs/src/main/app/custom-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Apply local tweaks if file mounted in container (only Kubernetes)
+echo "$0: - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+CUSTOMFILE=/customize/my-env.sh
+if [ -f $CUSTOMFILE ]
+then
+ echo "$0: apply $CUSTOMFILE" 
+ source $CUSTOMFILE
+else
+ echo "$0: no $CUSTOMFILE to apply" 
+fi
+echo "$0: - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+
+# Configure address for Kubernetes or Docker
+MC_EMPTY=''
+MC_CLUSTER1_LIST=`echo ${MC_CLUSTER1_ADDRESSLIST_OVERRIDE:-${MC_EMPTY}} $MC_CLUSTER1_ADDRESSLIST |awk '{print $1}'`
+
+# Run Node.js
+echo npm --loglevel=error start $MC_CLUSTER1_NAME $MC_CLUSTER1_LIST
+npm --loglevel=error start $MC_CLUSTER1_NAME $MC_CLUSTER1_LIST
