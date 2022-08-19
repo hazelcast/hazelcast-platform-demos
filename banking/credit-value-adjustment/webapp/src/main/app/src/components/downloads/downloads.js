a293c94dc620b30b351f0cda5ee552fe5c55dd3a
Neil Stevenson <neilstevenson@users.noreply.github.com>
2022-08-19 15:19:47 +0100

    Initial

--- banking/credit-value-adjustment/webapp/src/main/app/src/components/downloads/downloads.js
+++ banking/credit-value-adjustment/webapp/src/main/app/src/components/downloads/downloads.js
@@ -188,7 +188,6 @@ class Downloads extends Component {
         return (
         	<div>
     		    <h2>Downloads</h2>
-				<center><font size="small">Refreshes for local jobs</font></center>
         		<SockJsClient 
                 	url={WS_URL}
         			topics={WS_TOPICS}
