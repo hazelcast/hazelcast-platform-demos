b5b3d856139a21fbf530b70fff9ecfd1d2787d65
Neil Stevenson <neil@hazelcast.com>
2023-08-08 13:15:19 +0100

    Initial

--- banking/transaction-monitor/abstract-hazelcast-node/src/main/java/hazelcast/platform/demos/banking/transactionmonitor/ApplicationConfig.java
+++ banking/transaction-monitor/abstract-hazelcast-node/src/main/java/hazelcast/platform/demos/banking/transactionmonitor/ApplicationConfig.java
@@ -21,17 +21,14 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
-import java.util.Properties;
-
 import java.util.Map.Entry;
+import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.hazelcast.config.ClasspathYamlConfig;
 import com.hazelcast.config.Config;
-import com.hazelcast.config.DataConnectionConfig;
 import com.hazelcast.config.DataPersistenceConfig;
 import com.hazelcast.config.DiscoveryConfig;
 import com.hazelcast.config.DiscoveryStrategyConfig;
@@ -99,9 +96,6 @@ public class ApplicationConfig {
             LOGGER.info("Non-Kubernetes configuration: member-list: {}", tcpIpConfig.getMembers());
             LOGGER.info("Non-Kubernetes configuration: use port: {}", config.getNetworkConfig().getPort());
         }
-
-        //TODO Fix once supported by Viridian
-        temporaryDefineDataLink(config, properties);
 
         // If using Enterprise
         if (!config.getWanReplicationConfigs().isEmpty()) {
@@ -315,45 +309,4 @@ public class ApplicationConfig {
         }
     }
 
-    /**
-     * <p><b>Temporary coding</b></p>
-     * <p>Define an external store (MySql), for use by a map with generic
-     * map store.
-     * </p>
-     *
-     * TODO Once supported by Viridian, move to {@link TransactionMonitorIdempotentInitialization}/
-     *
-     * @param config To extend
-     * @param properties For logon, password
-     */
-    private static void temporaryDefineDataLink(Config config, Properties properties) {
-        LOGGER.warn("Add generic map-store only currently for self-hosted");
-
-        try {
-            TransactionMonitorFlavor transactionMonitorFlavor = MyUtils.getTransactionMonitorFlavor(properties);
-
-            String address = System.getProperty(MyConstants.MYSQL_ADDRESS);
-            String database = "transaction-monitor-" + transactionMonitorFlavor.toString().toLowerCase(Locale.ROOT);
-            String username = properties.getProperty("my.mysql.user");
-            String password = properties.getProperty("my.mysql.password");
-            String jdbcUrl = "jdbc:mysql://" + address + "/" + database;
-
-            LOGGER.info("MySql connection: Url: '{}'", jdbcUrl);
-            LOGGER.trace("MySql connection: User: '{}'", username);
-            LOGGER.trace("MySql connection: Pass: '{}'", password);
-
-            DataConnectionConfig dataConnectionConfig =
-                    new DataConnectionConfig(MyConstants.MYSQL_DATACONNECTION_CONFIG_NAME)
-                    .setType("jdbc")
-                    .setProperty("jdbcUrl", jdbcUrl)
-                    .setProperty("username", username)
-                    .setProperty("password", password)
-                    .setShared(true);
-
-            config.addDataConnectionConfig(dataConnectionConfig);
-
-        } catch (Exception e) {
-            LOGGER.error("temporaryDefineDataLink()", e);
-        }
-    }
 }
--- banking/transaction-monitor/src/main/scripts/docker-webapp.sh
+++ banking/transaction-monitor/src/main/scripts/docker-webapp.sh
@@ -29,6 +29,7 @@ then
 fi
 
 MY_BOOTSTRAP_SERVERS=kafka-broker0:9092,kafka-broker1:9093,kafka-broker2:9094
+MY_MYSQL_ADDRESS=mysql:3306
 MY_POSTGRES_ADDRESS=postgres:5432
 MY_PULSAR_LIST=pulsar:6650
 
@@ -42,6 +43,7 @@ docker container prune --force > /dev/null 2>&1
 # External port 8081
 CMD="docker run -e MY_BOOTSTRAP_SERVERS=$MY_BOOTSTRAP_SERVERS \
  -e MY_KUBERNETES_ENABLED=false \
+ -e MY_MYSQL_ADDRESS=$MY_MYSQL_ADDRESS \
  -e MY_POSTGRES_ADDRESS=$MY_POSTGRES_ADDRESS \
  -e MY_PULSAR_LIST=$MY_PULSAR_LIST \
  -e JAVA_ARGS=-Dhazelcast.local.publicAddress=${HOST_IP}:5701 \
