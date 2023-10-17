443058f9afa7b6e6f2dad3505397d94be7ee043d
Neil Stevenson <neil@hazelcast.com>
2023-10-17 11:41:30 +0100

    Initial

--- banking/transaction-monitor/webapp/src/main/app/src/index.js
+++ banking/transaction-monitor/webapp/src/main/app/src/index.js
@@ -1,7 +1,7 @@
 import React                    from 'react'
 import ReactDOM                 from 'react-dom'
 
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import App                      from './App'
 
@@ -11,7 +11,9 @@ import './index.css'
 
 ReactDOM.render(
     <BrowserRouter>
+      <Switch>
         <Route component={App}/>
+      </Switch>
     </BrowserRouter>,
     document.getElementById('root')
 )
